205f4283c6d46d4f21c9bdb101c5e3f4f8391db9
m-de-graaff <mark_degraaff@outlook.com>
2025-09-16 15:28:54 +0200

    fix(providers): use extensionless re-exports to satisfy TS dts build

--- packages/providers/src/index.ts
+++ packages/providers/src/index.ts
@@ -1,6 +1,6 @@
 // Re-export all providers
-export { github } from './github/index.js'
-export { default as githubProvider } from './github/index.js'
+export { github } from './github/index'
+export { default as githubProvider } from './github/index'
 
-export { default as google } from './google/index.js'
-export { default as discord } from './discord/index.js'
+export { default as google } from './google/index'
+export { default as discord } from './discord/index'
