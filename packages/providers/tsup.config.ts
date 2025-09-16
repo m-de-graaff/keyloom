66e27c4f00bd843a5c80a83c3b586e939e75dedd
m-de-graaff <mark_degraaff@outlook.com>
2025-09-16 15:27:20 +0200

    build(providers): include google/discord in tsup entries to fix dts build

--- packages/providers/tsup.config.ts
+++ packages/providers/tsup.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: {
     index: 'src/index.ts',
-    github: 'src/github/index.ts'
+    github: 'src/github/index.ts',
+    google: 'src/google/index.ts',
+    discord: 'src/discord/index.ts'
   },
   format: ['cjs', 'esm'],
   dts: true,
